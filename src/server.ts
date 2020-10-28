94eb28e166910769752f0c292f8c09fbbfdf8f03
b0ntenmaru <mmst3l4@gmail.com>
2020-10-28 12:04:29 +0900

    tslintの設定を修正

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import router from './contexts/user_context/infrastructure/router';
 import bodyParser from 'body-parser';
 import { getConnectionOptions, createConnection, BaseEntity } from 'typeorm';
 
-const app = async() => {
+const app = async () => {
   const express = Express();
   const port: number = 8080;
   express.use(bodyParser.urlencoded({ extended: true }));
@@ -12,14 +12,14 @@ const app = async() => {
   // --- TypeORMの設定
   const connectionOptions = await getConnectionOptions();
   const connection = await createConnection(connectionOptions);
-  BaseEntity.useConnection(connection)
+  BaseEntity.useConnection(connection);
   /**
    * routing呼び出し
    */
   express.use('/', router);
 
   express.listen(port, () => {
-    console.log('Started Express Process')
+    console.log('Started Express Process');
     console.log(`Runnning on http://localhost:${port}`);
   });
 };
